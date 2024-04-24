467415a23978f00c5a482ca1e8ccdd0393fdddfd
Larissa <Larissa.vsl@aluno.ifsc.edu.br>
2024-04-24 16:02:39 -0300

    Classes Conta e Endereco

--- GSC/src/Conta.java
+++ GSC/src/Conta.java
@@ -5,31 +5,37 @@ public class Conta extends Endereco {
 	private int numeroConta;
 	private int numeroTelefone;
 	private float valorSaldo;
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getNumeroConta() {
 		return numeroConta;
 	}
+
 	public void setNumeroConta(int numeroConta) {
 		this.numeroConta = numeroConta;
 	}
+
 	public int getNumeroTelefone() {
 		return numeroTelefone;
 	}
+
 	public void setNumeroTelefone(int numeroTelefone) {
 		this.numeroTelefone = numeroTelefone;
 	}
+
 	public float getValorSaldo() {
 		return valorSaldo;
 	}
+
 	public void setValorSaldo(float valorSaldo) {
 		this.valorSaldo = valorSaldo;
 	}
-	
 
 }
